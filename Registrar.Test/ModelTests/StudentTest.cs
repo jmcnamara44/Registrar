1ea609ed3c2320bbcc8fb7493126f6c5af7c7569
Austin Roberts and Jimmy McNamara <austin@>
2018-05-09 10:06:17 -0700

    allow the user to save a course to a specific student and get a list of students associated with a course.

--- Registrar.Test/ModelTests/StudentTest.cs
+++ Registrar.Test/ModelTests/StudentTest.cs
@@ -20,93 +20,44 @@ namespace RegistrarApp.Tests
         {
             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=registrar_test;";
         }
-        // [TestMethod]
-        // public void GetAll_DbStartsEmpty_0()
-        // {
-        //   //Arrange
-        //   //Act
-        //   int result = Student.GetAll().Count;
-        //
-        //   //Assert
-        //   Assert.AreEqual(0, result);
-        // }
-        // [TestMethod]
-        // public void Save_SavesToDatabase_StudentList()
-        // {
-        //
-        //   Student newStudent = new Student("Helen");
-        //   Student otherStudent = new Student("bob");
-        //   newStudent.Save();
-        //   otherStudent.Save();
-        //   List <Student> studentList = new List<Student>{newStudent, otherStudent};
-        //   List <Student> result = Student.GetAll();
-        //   CollectionAssert.AreEqual(studentList, result);
-        // }
-        // [TestMethod]
-        // public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
-        // {
-        //   // Arrange, Act
-        //   Item firstItem = new Item("Mow the lawn");
-        //   Item secondItem = new Item("Mow the lawn");
-        //
-        //   // Assert
-        //   Assert.AreEqual(firstItem, secondItem);
-        // }
-        // [TestMethod]
-        // public void Save_SavesToDatabase_ItemList()
-        // {
-        //   //Arrange
-        //   Item testItem = new Item("Mow the lawn");
-        //
-        //   //Act
-        //   testItem.Save();
-        //   List<Item> result = Item.GetAll();
-        //   Console.WriteLine(result.Count);
-        //   foreach(Item element in result)
-        //   {
-        //     Console.WriteLine("result: " + result);
-        //   }
-        //
-        //
-        //   List<Item> testList = new List<Item>{testItem};
-        //   Console.WriteLine(testList.Count);
-        //   Console.WriteLine("testItem "+testItem);
-        //   Console.WriteLine("testList " +testList);
-        //   //Assert
-        //   CollectionAssert.AreEqual(testList, result);
-        // }
+        [TestMethod]
+        public void GetAll_DbStartsEmpty_0()
+        {
+          //Arrange
+          //Act
+          int result = Student.GetAll().Count;
 
-        // [TestMethod]
-        // public void Save_AssignsIdToObject_Id()
-        // {
-        //   //Arranges
-        //   Item testItem = new Item("Mow the lawn");
-        //
-        //   //Act
-        //   testItem.Save();
-        //   Item savedItem = Item.GetAll()[0];
-        //
-        //   int result = savedItem.GetId();
-        //   int testId = testItem.GetId();
-        //
-        //   Console.WriteLine("testId: " + testId);
-        //   Console.WriteLine("result: " + result);
-        //   //Assert
-        //   Assert.AreEqual(testId, result);
-        // }
-        // [TestMethod]
-        // public void Find_FindsItemInDatabase_Item()
-        // {
-        //   //Arrange
-        //   Item testItem = new Item("Mow the lawn");
-        //   testItem.Save();
-        //
-        //   //Act
-        //   Item foundItem = Item.Find(testItem.GetId());
-        //
-        //   //Assert
-        //   Assert.AreEqual(testItem, foundItem);
-        // }
+          //Assert
+          Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Save_SavesToDatabase_StudentList()
+        {
+
+          Student newStudent = new Student("Helen");
+          Student otherStudent = new Student("bob");
+          newStudent.Save();
+          otherStudent.Save();
+          List <Student> studentList = new List<Student>{newStudent, otherStudent};
+          List <Student> result = Student.GetAll();
+          CollectionAssert.AreEqual(studentList, result);
+        }
+        [TestMethod]
+        public void GetCourses_PullsFromCourseTable_CourseList()
+        {
+          Course newCourse = new Course("math");
+          newCourse.Save();
+          Student newStudent = new Student("Jimmy");
+          newStudent.Save();
+          newStudent.AddCourse(newCourse);
+
+          Console.WriteLine("newstudent id: " + newStudent.GetId());
+          Console.WriteLine("newcourse id: " + newCourse.GetId());
+
+          List <Course> courseList = new List<Course>{newCourse};
+          List <Course> DBcourseList = newStudent.GetCourses();
+          CollectionAssert.AreEqual(courseList, DBcourseList);
+        }
 
     }
 }
--- Registrar/Models/Student.cs
+++ Registrar/Models/Student.cs
@@ -83,6 +83,54 @@ namespace RegistrarApp.Models
           conn.Dispose();
       }
     }
+    public void AddCourse(Course newCourse)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO course_student (course_id, student_id) VALUES (@courseId, @studentId);";
+
+      cmd.Parameters.Add(new MySqlParameter("@courseId", newCourse.GetId()));
+      cmd.Parameters.Add(new MySqlParameter("@studentId", _id));
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public List<Course> GetCourses()
+    {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT courses.* FROM students
+            JOIN course_student ON (students.id = course_student.student_id)
+            JOIN courses ON (course_student.course_id = courses.id)
+            WHERE students.id = @StudentId;";
+
+        MySqlParameter studentIdParameter = new MySqlParameter();
+        studentIdParameter.ParameterName = "@StudentId";
+        studentIdParameter.Value = _id;
+        cmd.Parameters.Add(studentIdParameter);
+
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        List<Course> courses = new List<Course>{};
+
+        while(rdr.Read())
+        {
+          int courseId = rdr.GetInt32(0);
+          string courseName = rdr.GetString(1);
+          Course newCourse = new Course(courseName, courseId);
+          courses.Add(newCourse);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return courses;
+    }
     public static void DeleteAll()
     {
       MySqlConnection conn = DB.Connection();
